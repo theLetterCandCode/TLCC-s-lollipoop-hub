5391f04bbbf63a0ff9cb144f52e1a24dae6eefdb
theLetterCandCode <162311084+theLetterCandCode@users.noreply.github.com>
2025-10-04 02:14:00 -0400

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -1,6 +1,13 @@
+--[=[
+ d888b  db    db d888888b      .d888b.      db      db    db  .d8b.  
+88' Y8b 88    88   `88'        VP  `8D      88      88    88 d8' `8b 
+88      88    88    88            odD'      88      88    88 88ooo88 
+88  ooo 88    88    88          .88'        88      88    88 88~~~88 
+88. ~8~ 88b  d88   .88.        j88.         88booo. 88b  d88 88   88    @uniquadev
+ Y888P  ~Y8888P' Y888888P      888888D      Y88888P ~Y8888P' YP   YP  CONVERTER 
+]=]
 
-
--- Instances: 11 | Scripts: 5 | Modules: 0 | Tags: 0
+-- Instances: 21 | Scripts: 5 | Modules: 0 | Tags: 0
 local G2L = {};
 
 -- StarterGui.ScreenGui
@@ -11,95 +18,290 @@ G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
 -- StarterGui.ScreenGui.Frame
 G2L["2"] = Instance.new("Frame", G2L["1"]);
 G2L["2"]["BorderSizePixel"] = 0;
-G2L["2"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
-G2L["2"]["Size"] = UDim2.new(0, 480, 0, 271);
-G2L["2"]["Position"] = UDim2.new(0.24142, 0, 0.55546, 0);
+G2L["2"]["BackgroundColor3"] = Color3.fromRGB(92, 92, 92);
+G2L["2"]["Size"] = UDim2.new(0, 828, 0, 266);
+G2L["2"]["Position"] = UDim2.new(0.23691, 0, 0.38852, 0);
 G2L["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
 
 
--- StarterGui.ScreenGui.Frame.Drag
-G2L["3"] = Instance.new("LocalScript", G2L["2"]);
-G2L["3"]["Name"] = [[Drag]];
-
-
 -- StarterGui.ScreenGui.Frame.TextLabel
-G2L["4"] = Instance.new("TextLabel", G2L["2"]);
-G2L["4"]["TextWrapped"] = true;
-G2L["4"]["BorderSizePixel"] = 0;
-G2L["4"]["TextSize"] = 14;
-G2L["4"]["TextScaled"] = true;
-G2L["4"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
-G2L["4"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
-G2L["4"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["4"]["Size"] = UDim2.new(0, 340, 0, 50);
-G2L["4"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["4"]["Text"] = [[Lollipoop Hub]];
-G2L["4"]["Position"] = UDim2.new(0.18958, 0, 0.04059, 0);
+G2L["3"] = Instance.new("TextLabel", G2L["2"]);
+G2L["3"]["TextWrapped"] = true;
+G2L["3"]["BorderSizePixel"] = 0;
+G2L["3"]["TextSize"] = 14;
+G2L["3"]["TextScaled"] = true;
+G2L["3"]["BackgroundColor3"] = Color3.fromRGB(3, 142, 47);
+G2L["3"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["3"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["3"]["RichText"] = true;
+G2L["3"]["Size"] = UDim2.new(0, 828, 0, 50);
+G2L["3"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["3"]["Text"] = [[Tree hub]];
 
 
--- StarterGui.ScreenGui.Frame.ImageLabel
-G2L["5"] = Instance.new("ImageLabel", G2L["2"]);
-G2L["5"]["BorderSizePixel"] = 0;
-G2L["5"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
--- [ERROR] cannot convert ImageContent, please report to "https://github.com/uniquadev/GuiToLuaConverter/issues"
-G2L["5"]["Image"] = [[rbxassetid://10152260925]];
-G2L["5"]["Size"] = UDim2.new(0, 100, 0, 100);
-G2L["5"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+-- StarterGui.ScreenGui.Frame.TextLabel.UIStroke
+G2L["4"] = Instance.new("UIStroke", G2L["3"]);
+G2L["4"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+G2L["4"]["Color"] = Color3.fromRGB(255, 255, 255);
 
 
--- StarterGui.ScreenGui.Frame.ImageLabel.LocalScript
-G2L["6"] = Instance.new("LocalScript", G2L["5"]);
+-- StarterGui.ScreenGui.Frame.UIStroke
+G2L["5"] = Instance.new("UIStroke", G2L["2"]);
+G2L["5"]["Color"] = Color3.fromRGB(255, 255, 255);
 
 
+-- StarterGui.ScreenGui.Frame.t1
+G2L["6"] = Instance.new("TextBox", G2L["2"]);
+G2L["6"]["CursorPosition"] = -1;
+G2L["6"]["Name"] = [[t1]];
+G2L["6"]["BorderSizePixel"] = 0;
+G2L["6"]["TextWrapped"] = true;
+G2L["6"]["TextSize"] = 14;
+G2L["6"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["6"]["TextScaled"] = true;
+G2L["6"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["6"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["6"]["PlaceholderText"] = [[character]];
+G2L["6"]["Size"] = UDim2.new(0, 300, 0, 50);
+G2L["6"]["Position"] = UDim2.new(0.01615, 0, 0.29737, 0);
+G2L["6"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["6"]["Text"] = [[]];
 
--- StarterGui.ScreenGui.Frame.TextButton
-G2L["7"] = Instance.new("TextButton", G2L["2"]);
-G2L["7"]["TextWrapped"] = true;
+
+-- StarterGui.ScreenGui.Frame.t2
+G2L["7"] = Instance.new("TextBox", G2L["2"]);
+G2L["7"]["CursorPosition"] = -1;
+G2L["7"]["Name"] = [[t2]];
 G2L["7"]["BorderSizePixel"] = 0;
+G2L["7"]["TextWrapped"] = true;
 G2L["7"]["TextSize"] = 14;
-G2L["7"]["TextScaled"] = true;
 G2L["7"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["7"]["TextScaled"] = true;
 G2L["7"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
 G2L["7"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
-G2L["7"]["Size"] = UDim2.new(0, 226, 0, 95);
+G2L["7"]["PlaceholderText"] = [[skin name]];
+G2L["7"]["Size"] = UDim2.new(0, 300, 0, 50);
+G2L["7"]["Position"] = UDim2.new(0.01615, 0, 0.55902, 0);
 G2L["7"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["7"]["Text"] = [[Buy all BFDIA characters]];
-G2L["7"]["Position"] = UDim2.new(0.26667, 0, 0.42804, 0);
-
-
--- StarterGui.ScreenGui.Frame.TextButton.LocalScript
-G2L["8"] = Instance.new("LocalScript", G2L["7"]);
-
+G2L["7"]["Text"] = [[]];
 
 
 -- StarterGui.ScreenGui.Frame.TextButton
-G2L["9"] = Instance.new("TextButton", G2L["2"]);
-G2L["9"]["TextWrapped"] = true;
-G2L["9"]["BorderSizePixel"] = 0;
-G2L["9"]["TextSize"] = 14;
-G2L["9"]["TextScaled"] = true;
-G2L["9"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["9"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
-G2L["9"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
-G2L["9"]["Size"] = UDim2.new(0, 226, 0, 45);
-G2L["9"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["9"]["Text"] = [[Bye dumbass]];
-G2L["9"]["Position"] = UDim2.new(0.26458, 0, 0.83395, 0);
+G2L["8"] = Instance.new("TextButton", G2L["2"]);
+G2L["8"]["TextWrapped"] = true;
+G2L["8"]["BorderSizePixel"] = 0;
+G2L["8"]["TextSize"] = 14;
+G2L["8"]["TextScaled"] = true;
+G2L["8"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["8"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["8"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["8"]["Size"] = UDim2.new(0, 200, 0, 50);
+G2L["8"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["8"]["Text"] = [[Give skin]];
+G2L["8"]["Position"] = UDim2.new(0.0666, 0, 0.8, 0);
 
 
 -- StarterGui.ScreenGui.Frame.TextButton.LocalScript
-G2L["a"] = Instance.new("LocalScript", G2L["9"]);
+G2L["9"] = Instance.new("LocalScript", G2L["8"]);
 
 
 
--- StarterGui.ScreenGui.Frame.idc
-G2L["b"] = Instance.new("LocalScript", G2L["2"]);
-G2L["b"]["Name"] = [[idc]];
+-- StarterGui.ScreenGui.Frame.TextButton.Sound
+G2L["a"] = Instance.new("Sound", G2L["8"]);
+G2L["a"]["Volume"] = 1;
+G2L["a"]["RollOffMode"] = Enum.RollOffMode.InverseTapered;
+G2L["a"]["SoundId"] = [[rbxassetid://6895079853]];
 
 
--- StarterGui.ScreenGui.Frame.Drag
-local function C_3()
-local script = G2L["3"];
+-- StarterGui.ScreenGui.Frame.TextButton
+G2L["b"] = Instance.new("TextButton", G2L["2"]);
+G2L["b"]["TextWrapped"] = true;
+G2L["b"]["BorderSizePixel"] = 0;
+G2L["b"]["TextSize"] = 14;
+G2L["b"]["TextScaled"] = true;
+G2L["b"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["b"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["b"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["b"]["Size"] = UDim2.new(0, 382, 0, 50);
+G2L["b"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["b"]["Text"] = [[All charatcers unlocked]];
+G2L["b"]["Position"] = UDim2.new(0.41355, 0, 0.29474, 0);
+
+
+-- StarterGui.ScreenGui.Frame.TextButton.LocalScript
+G2L["c"] = Instance.new("LocalScript", G2L["b"]);
+
+
+
+-- StarterGui.ScreenGui.Frame.TextButton.Sound
+G2L["d"] = Instance.new("Sound", G2L["b"]);
+G2L["d"]["Volume"] = 1;
+G2L["d"]["RollOffMode"] = Enum.RollOffMode.InverseTapered;
+G2L["d"]["SoundId"] = [[rbxassetid://6895079853]];
+
+
+-- StarterGui.ScreenGui.Frame.TextButton
+G2L["e"] = Instance.new("TextButton", G2L["2"]);
+G2L["e"]["TextWrapped"] = true;
+G2L["e"]["BorderSizePixel"] = 0;
+G2L["e"]["TextSize"] = 14;
+G2L["e"]["TextScaled"] = true;
+G2L["e"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["e"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["e"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["e"]["Size"] = UDim2.new(0, 104, 0, 49);
+G2L["e"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["e"]["Text"] = [[X]];
+G2L["e"]["Position"] = UDim2.new(0.86403, 0, 0.79699, 0);
+
+
+-- StarterGui.ScreenGui.Frame.TextButton.LocalScript
+G2L["f"] = Instance.new("LocalScript", G2L["e"]);
+
+
+
+-- StarterGui.ScreenGui.Frame.TextButton.Sound
+G2L["10"] = Instance.new("Sound", G2L["e"]);
+G2L["10"]["Volume"] = 1;
+G2L["10"]["RollOffMode"] = Enum.RollOffMode.InverseTapered;
+G2L["10"]["SoundId"] = [[rbxassetid://6895079853]];
+
+
+-- StarterGui.ScreenGui.Frame.TextButton
+G2L["11"] = Instance.new("TextButton", G2L["2"]);
+G2L["11"]["TextWrapped"] = true;
+G2L["11"]["BorderSizePixel"] = 0;
+G2L["11"]["TextSize"] = 14;
+G2L["11"]["TextScaled"] = true;
+G2L["11"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["11"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["11"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["11"]["Size"] = UDim2.new(0, 187, 0, 50);
+G2L["11"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["11"]["Text"] = [[Roll]];
+G2L["11"]["Position"] = UDim2.new(0.41355, 0, 0.55789, 0);
+
+
+-- StarterGui.ScreenGui.Frame.TextButton.LocalScript
+G2L["12"] = Instance.new("LocalScript", G2L["11"]);
+
+
+
+-- StarterGui.ScreenGui.Frame.TextButton.Sound
+G2L["13"] = Instance.new("Sound", G2L["11"]);
+G2L["13"]["Volume"] = 1;
+G2L["13"]["RollOffMode"] = Enum.RollOffMode.InverseTapered;
+G2L["13"]["SoundId"] = [[rbxassetid://6895079853]];
+
+
+-- StarterGui.ScreenGui.Frame.drag
+G2L["14"] = Instance.new("LocalScript", G2L["2"]);
+G2L["14"]["Name"] = [[drag]];
+
+
+-- StarterGui.ScreenGui.Frame.t3
+G2L["15"] = Instance.new("TextBox", G2L["2"]);
+G2L["15"]["CursorPosition"] = -1;
+G2L["15"]["Name"] = [[t3]];
+G2L["15"]["BorderSizePixel"] = 0;
+G2L["15"]["TextWrapped"] = true;
+G2L["15"]["TextSize"] = 14;
+G2L["15"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["15"]["TextScaled"] = true;
+G2L["15"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["15"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["15"]["PlaceholderText"] = [[1-?]];
+G2L["15"]["Size"] = UDim2.new(0, 187, 0, 50);
+G2L["15"]["Position"] = UDim2.new(0.41349, 0, 0.79962, 0);
+G2L["15"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["15"]["Text"] = [[]];
+
+
+-- StarterGui.ScreenGui.Frame.TextButton.LocalScript
+local function C_9()
+local script = G2L["9"];
+	script.Parent.MouseButton1Up:Connect(function()
+		script.Parent.Sound:Play()
+		local r1 = script.Parent.Parent.t1.Text
+		local r2 = script.Parent.Parent.t2.Text
+		local browhat = game:GetService("Players").LocalPlayer.PlayerGui
+		browhat.NewShop.TotalFrame.BagFrame.MorphSkinFrame:WaitForChild(r1):WaitForChild("Skin25"):WaitForChild("SkinVal").Value = r2
+		browhat.NewShop.TotalFrame.BagFrame.MorphSkinFrame:WaitForChild(r1):WaitForChild("Skin25").Visible = true
+	end)
+end;
+task.spawn(C_9);
+-- StarterGui.ScreenGui.Frame.TextButton.LocalScript
+local function C_c()
+local script = G2L["c"];
+	script.Parent.MouseButton1Up:Connect(function()
+		script.Parent.Sound:Play()
+		local lclplr = game:GetService("Players").LocalPlayer.PlayerGui
+		lclplr.Morphs.Frame.MorphBFDI1.Blocky.Bought.Value = true
+		lclplr.Morphs.Frame.MorphBFDI1.Flower.Bought.Value = true
+		lclplr.Morphs.Frame.MorphBFDI1.David.Bought.Value = true
+		lclplr.Morphs.Frame.MorphBFDI1.Wood.Bought.Value = true
+		lclplr.Morphs.Frame.MorphBFDI1.Snowball.Bought.Value = true
+		lclplr.Morphs.Frame.MorphBFDI1.Pen.Bought.Value = true
+		lclplr.Morphs.Frame.MorphBFDI1.Leafy.Bought.Value = true
+		lclplr.Morphs.Frame.MorphBFDI1.Eraser.Bought.Value = true
+		lclplr.Morphs.Frame.MorphBFDI1.Bubble.Bought.Value = true
+		print("BFDI characters enabled")
+		task.wait(0.01)
+		lclplr.Morphs.Frame.MorphIDFB.Balloony.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Basketball.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Bell.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Blackhole.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Blehbag.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Bottle.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Bracelety.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Cake.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Cloudy.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Eggy.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Eightball.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Fanny.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.FireyJr.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Foldy.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Gaty.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Grassy.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Lightning.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Liy.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Lollipop.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Loser.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Marker.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.RobotFlower.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Roboty.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Remote.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Saw.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Stapy.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.TV.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Taco.Bought.Value = true
+		lclplr.Morphs.Frame.MorphIDFB.Tree.Bought.Value = true
+		print("IDFB/BFB characters enabled")
+	end)
+end;
+task.spawn(C_c);
+-- StarterGui.ScreenGui.Frame.TextButton.LocalScript
+local function C_f()
+local script = G2L["f"];
+	script.Parent.MouseButton1Up:Connect(function()
+		script.Parent.Parent.Parent:Destroy()
+	end)
+end;
+task.spawn(C_f);
+-- StarterGui.ScreenGui.Frame.TextButton.LocalScript
+local function C_12()
+local script = G2L["12"];
+	script.Parent.MouseButton1Up:Connect(function()
+		local lastdigit = script.Parent.Parent.t3.Text
+		script.Parent.Sound:Play()
+		local selected = math.random(1, lastdigit)
+		script.Parent.Text = selected
+	end)
+end;
+task.spawn(C_12);
+-- StarterGui.ScreenGui.Frame.drag
+local function C_14()
+local script = G2L["14"];
 	local UserInputService = game:GetService("UserInputService")
 	local runService = (game:GetService("RunService"));
 	
@@ -155,107 +357,6 @@ local script = G2L["3"];
 	runService.Heartbeat:Connect(Update)
 	
 end;
-task.spawn(C_3);
--- StarterGui.ScreenGui.Frame.ImageLabel.LocalScript
-local function C_6()
-local script = G2L["6"];
-	while true do
-		task.wait(0.1)
-		script.Parent.Image = "rbxassetid://5656936001"
-		task.wait(0.1)
-		script.Parent.Image = "rbxassetid://10152260925"
-		task.wait(0.1)
-		script.Parent.Image = "rbxassetid://15817441076"
-	end
-end;
-task.spawn(C_6);
--- StarterGui.ScreenGui.Frame.TextButton.LocalScript
-local function C_8()
-local script = G2L["8"];
-	script.Parent.MouseButton1Up:Connect(function()
-		local lclplr = game:GetService("Players").LocalPlayer.PlayerGui
-		lclplr.Morphs.Frame.MorphBFDI1.Blocky.Bought.Value = true
-		lclplr.Morphs.Frame.MorphBFDI1.Flower.Bought.Value = true
-		lclplr.Morphs.Frame.MorphBFDI1.David.Bought.Value = true
-		lclplr.Morphs.Frame.MorphBFDI1.Wood.Bought.Value = true
-		lclplr.Morphs.Frame.MorphBFDI1.Snowball.Bought.Value = true
-		lclplr.Morphs.Frame.MorphBFDI1.Pen.Bought.Value = true
-		lclplr.Morphs.Frame.MorphBFDI1.Leafy.Bought.Value = true
-		lclplr.Morphs.Frame.MorphBFDI1.Eraser.Bought.Value = true
-		lclplr.Morphs.Frame.MorphBFDI1.Bubble.Bought.Value = true
-		print("BFDI characters enabled")
-		task.wait(0.01)
-		lclplr.Morphs.Frame.MorphIDFB.Balloony.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Basketball.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Bell.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Blackhole.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Blehbag.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Bottle.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Bracelety.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Cake.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Cloudy.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Eggy.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Eightball.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Fanny.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.FireyJr.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Foldy.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Gaty.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Grassy.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Lightning.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Liy.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Lollipop.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Loser.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Marker.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.RobotFlower.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Roboty.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Remote.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Saw.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Stapy.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.TV.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Taco.Bought.Value = true
-		lclplr.Morphs.Frame.MorphIDFB.Tree.Bought.Value = true
-		print("IDFB/BFB characters enabled")
-	end)
-end;
-task.spawn(C_8);
--- StarterGui.ScreenGui.Frame.TextButton.LocalScript
-local function C_a()
-local script = G2L["a"];
-	script.Parent.MouseButton1Up:Connect(function()
-		script.Parent.Parent.Parent:Destroy()
-	end)
-end;
-task.spawn(C_a);
--- StarterGui.ScreenGui.Frame.idc
-local function C_b()
-local script = G2L["b"];
-	while wait() do
-		for i = 0,255,5 do
-			script.Parent.BackgroundColor3 = Color3.fromRGB(255,i,0) -- change backgroundcolor3 to textcolor3 to make text color rainbow
-			wait(0.01)
-		end
-		for i = 255,0,-5 do
-			script.Parent.BackgroundColor3 = Color3.fromRGB(i,255,0)
-			wait(0.01)
-		end
-		for i = 0,255,5 do
-			script.Parent.BackgroundColor3 = Color3.fromRGB(0,255,i)
-			wait(0.01)
-		end
-		for i = 255,0,-5 do
-			script.Parent.BackgroundColor3 = Color3.fromRGB(0,i,255)
-			wait(0.01)
-		end
-		for i = 0,255,5 do
-			script.Parent.BackgroundColor3 = Color3.fromRGB(i,0,255)
-			wait(0.01)
-		end
-		for i = 255,0,-5 do
-			script.Parent.BackgroundColor3 = Color3.fromRGB(255,0,i)
-			wait(0.01)
-		end
-	end
-end;
-task.spawn(C_b);
+task.spawn(C_14);
 
 return G2L["1"], require;
